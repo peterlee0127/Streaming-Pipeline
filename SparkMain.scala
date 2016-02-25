3e05a2b35de763e909ded804546632ef647cf1ab
Peterlee <peterlee0127@gmail.com>
2016-02-25 23:18:25 +0800

    use DataFrame API

--- SparkMain.scala
+++ SparkMain.scala
@@ -2,17 +2,33 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.Logging
+import org.apache.spark.sql
+import org.apache.log4j._
 
 object SimpleApp {
+  def setLogger() = {
+    Logger.getLogger("org.apache.kafka").setLevel(Level.OFF)
+    Logger.getLogger("org.apache.spark").setLevel(Level.OFF)
+    Logger.getLogger("org.apache.log4j").setLevel(Level.OFF)
+    Logger.getLogger("kafka.utils").setLevel(Level.OFF)
+    Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
+    Logger.getLogger("com.github.fommil.netlib").setLevel(Level.OFF)
+  }
+
   def main(args: Array[String]) {
-    val logFile = "README.md" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application")
+    setLogger()
+    val logFile = "noFilterTweets.json" // Should be some file on your system
+    val conf = new SparkConf().setAppName("SparkCore")
     val sc = new SparkContext(conf)
-//    val logData = sc.textFile(logFile, 2).cache()
-    val files = sc.addFile("https://raw.githubusercontent.com/peterlee0127/iBeacon_AutoCheckIn/master/README.md")
-    val logData = sc.textFile("README.md")
-    val numAs = logData.filter(line => line.contains("iOS")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with iOS: %s, Lines with b: %s".format(numAs, numBs))
+
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+    val df = sqlContext.read.json(logFile)
+    df.groupBy("lang").count().sort().show()
+
+    people.registerTempTable("tweet")
+    val teenagers = sqlContext.sql("SELECT name FROM people WHERE age >= 13 AND age <= 19")
+
   }
 }
