a06542f3f1a6e3649499c1de05c9e479f8436bd0
Peterlee <peterlee0127@gmail.com>
2016-05-29 23:11:54 +0900

    fix

--- SparkCore.scala
+++ SparkCore.scala
@@ -1,10 +1,11 @@
 /* SimpleApp.scala */
+import org.apache.log4j._
+import org.apache.spark.Logging
+import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-import org.apache.spark.Logging
 import org.apache.spark.sql
-import org.apache.log4j._
+import scala.collection.mutable.WrappedArray
 
 object SparkCore {
   def setLogger() = {
@@ -21,14 +22,44 @@ object SparkCore {
     val sc = new SparkContext(conf)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-    val logFile = "hdfs://hdfs:9000/aizu/dayTweets.json" // Should be some file on your system
-
+    //val logFile = "hdfs://hdfs:9000/aizu/dayTweets.json" // Should be some file on your system
+    val logFile = "rooms.json"
     val df = sqlContext.read.json(logFile)
     // df.printSchema()
-    df.groupBy("lang").count().show()
+    df.registerTempTable("rooms")
+    val result = sqlContext.sql("SELECT payload FROM rooms WHERE payload IS NOT NULL")
+    result.persist()
+    result.printSchema()
 
-    // people.registerTempTable("tweet")
-    // val teenagers = sqlContext.sql("SELECT l FROM tweet WHERE age >= 13 AND age <= 19")
+//    val hashtags = result.select("text").map(_.getSeq[Seq[String]](0))
+  //  hashtags.map(println("row:"+_))
+//    hashtags.foreach(tag=>
+//        tag.foreach(_.rdd.map(word => (word(0), 1)).reduceByKey(_+_).foreach(println))
+//    )
+//    hashtags.collect().flatMap(word => (word(0), 1)).reduceByKey(_+_).foreach(println)
+    // .toJavaRDD.map(word => (word, 1)).reduceByKey(_+_).foreach(println)
+    // .collect()
+    // .map(_.getAs[Seq[String]](0)).foreach(
+    //     _.foreach(item=>
+    //         item.map(word => (word, 1)).reduceByKey(_+_).foreach(println)
+    //         // item
+    //     )
+    // )
+    // .flatMap(println(_))
+        //_.getAs[Seq[String]](0))
+    // hashtags.foreach(println(_))
+    // val hashtagsArray:WrappedArray[String] = hashtags
+    //.getAs[Seq[String]](0)
+    //flatMap(word => (word, 1)).reduceByKey(_+_)
+    // hashtagsArray.flatMap(item=> println(item))
+    // foreach(println)
+
+    // flatMap(word => (word, 1)).reduceByKey(_+_).foreach(println)
+    //.count().show(100)//.foreach(println)
+
+
+    df.groupBy("payload.country").count().sort("count").show(300)
+    //df.groupBy("place.country").count().sort("count").show(50)
 
   }
 }
