4c5f537b0412e92de93c27b3d6654852bea56197
PeterLee <peterlee0127@gmail.com>
2016-09-10 21:32:35 +0900

    update

--- src/main/scala/Forest.scala
+++ src/main/scala/Forest.scala
@@ -5,11 +5,9 @@
 
 import org.apache.log4j._
 import org.apache.spark.ml.{Pipeline, PredictionModel, Predictor}
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame}
 import org.apache.spark.ml.feature._
 import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
-import org.apache.spark.ml.classification.RandomForestClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
 
@@ -17,16 +15,20 @@ object Forest {
 
   def transformData(data:DataFrame): DataFrame = {
 
-
-    val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
+    val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("token")
     val wordsData = tokenizer.transform(data)
-    val hashingTF = new HashingTF().setInputCol("words").setOutputCol("rawFeatures")
-    val featurizedData = hashingTF.transform(wordsData)
+
+    val remover = new StopWordsRemover().setInputCol("token").setOutputCol("filtered")
+    val cleanWords = remover.transform(wordsData)
+
+    val hashingTF = new HashingTF().setInputCol("filtered").setOutputCol("rawFeatures")
+    val featurizedData = hashingTF.transform(cleanWords)
 
     val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
     val idfModel = idf.fit(featurizedData)
     val resultData = idfModel.transform(featurizedData)
 
+    return resultData
 /*
       val transformers = Array(
       new StringIndexer().setInputCol("text").setOutputCol("label"),
@@ -34,26 +36,24 @@ object Forest {
       new CountVectorizer().setInputCol("tokens").setOutputCol("features")
     )
 
-
 val rf = new RandomForestClassifier()
 .setLabelCol("label")
 .setFeaturesCol("features")
 
 val model = new Pipeline().setStages(transformers :+ rf).fit(sentenceData)
 val prediction = model.transform(testData)
-prediction.select("tokens").show(false)
-prediction.select("probability","prediction").show(false)
+    prediction.select("tokens").show(false)
+    prediction.select("probability","prediction").show(false)
 
-*/
+    */
 
-    return resultData
+    //    return resultData
   }
   def trainModel(trainData:DataFrame):RandomForestClassificationModel =  {
 
     val forest = new RandomForestClassifier()
-      .setNumTrees(5)
       .setFeatureSubsetStrategy("auto")
-      .setSeed(5043)
+//      .setSeed(5043)
     val model = forest.fit(trainData)
 
     return model
--- src/main/scala/SparkCore.scala
+++ src/main/scala/SparkCore.scala
@@ -1,12 +1,16 @@
 /* SimpleApp.scala */
 
-
+import org.apache.spark.sql.functions._ // for `when`
 import org.apache.log4j._
 import org.apache.spark.ml.{PredictionModel, Predictor}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
+import org.apache.spark.ml.feature._
 import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+
+import org.apache.spark.ml.feature.{Tokenizer, CountVectorizer, StringIndexer}
+import org.apache.spark.ml.Pipeline
 
 object SparkCore {
   def setLogger() = {
@@ -17,37 +21,55 @@ object SparkCore {
   }
   def main(args: Array[String]) = {
     setLogger()
-//    val conf = new SparkConf().setMaster("local[4]").setAppName("Spark")
-//    val sc = new SparkContext(conf)
-//    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-//    import sqlContext.implicits._
+    val conf = new SparkConf().setMaster("local[4]").setAppName("Spark")
+    val sc = new SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    import sqlContext.implicits._
     val spark = SparkSession
       .builder().master("local[4]").appName("Spark").getOrCreate()
 
-//    val travel = spark.read.textFile("./../News-Tool/data/travel/").toDF()
-//    val money = spark.read.textFile("./../News-Tool/data/money/").toString()
-//    travel.printSchema()
+    val travel = spark.read.textFile("./../News-Tool/data/travel/").toDF("sentence")
+    val travelDF = travel.withColumn("label",  when($"sentence".isNotNull, 0.0))
 
-    val sentenceData = spark.createDataFrame(Seq(
-      (0.0,"orange black color white yellow kuro brown pink blue green"),
-      (1.0,"pc mac iphone watch notebook desktop-pc phone android"),
-      (2.0,"hadoop spark big-data data flink apache open-source"),
-      (3.0,"taiwan china hong-kong japan usa uk us america korea"),
-      (4.0,"dog cat man monkey pig cow sheep bug")
-    )).toDF("label", "sentence")
+    val money = spark.read.textFile("./../News-Tool/data/money/").toDF("sentence")
+    val moneyDF = money.withColumn("label",  when($"sentence".isNotNull, 1.0))
+    println("travel:"+travel.count()+"\nmoney:"+money.count())
 
-    val trainingData = Forest.transformData(sentenceData)
 
-    val model  = Forest.trainModel(trainingData)
+    val sentenceData = travelDF.union(moneyDF)
+    val Array(trainingSet,testSet) = sentenceData.randomSplit(Array(0.8,0.2))
 
-    val test = spark.createDataFrame(Seq(
-      (0.0, "dog")
-    )).toDF("label", "sentence")
+//    val trainedData = Forest.transformData(trainingSet)
+//    val model  = Forest.trainModel(trainedData)
+//    model.write.overwrite().save("model")
+//    val model = RandomForestClassificationModel.load("model")
 
-    val testData = Forest.transformData(test)
-    val prediction = model.transform(testData)
-    prediction.select("probability","prediction").show(false)
+    val transformers = Array(
+//      new StringIndexer().setInputCol("group").setOutputCol("label"),
+      new Tokenizer().setInputCol("sentence").setOutputCol("tokens"),
+      new StopWordsRemover().setInputCol("tokens").setOutputCol("filtered"),
+      new CountVectorizer().setInputCol("filtered").setOutputCol("features")
+    )
+    val rf = new RandomForestClassifier()
+      .setLabelCol("label")
+      .setFeaturesCol("features")
 
+    val model = new Pipeline().setStages(transformers :+ rf).fit(trainingSet)
+    val prediction = model.transform(testSet)
+
+//    val testedData = Forest.transformData(testSet)
+//    val prediction = model.transform(testedData)
+    prediction.select("probability","label","prediction").show(false)
+
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("label")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(prediction)
+    println("Test Error = " + (1.0 - accuracy))
+
+//    val rfModel = model.stages(2).asInstanceOf[RandomForestClassificationModel]
+//    println("Learned classification forest model:\n" + rfModel.toDebugString)
   }
 
 }
